7ce34d7a141c14f7ef14cdab2bd84f0775f2a679
YourGitHubUsername <n.j.thomas1991@gmail.com>
2026-02-02 11:32:55 +0000

    Add teacher signup page and route

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import home_page_view, hello, teacher_dashboard_view
+from .views import home_page_view, hello, teacher_dashboard_view, teacher_signup_view
 
 urlpatterns = [
     path("", home_page_view),
     path("api/hello/", hello),
+    path("teacher/signup/", teacher_signup_view, name="signup"),
     path("teacher/login/", auth_views.LoginView.as_view(template_name="core/teacher_login.html"), name="teacher_login"),
     path("teacher/", teacher_dashboard_view, name="teacher_dashboard"),
 ]
