53ca49780f61653f771fa93d768fdd421aac1a80
YourGitHubUsername <n.j.thomas1991@gmail.com>
2026-02-23 14:06:20 +0000

    Apply cleanup and auto-fixes; prepare for run

--- quest_app/views.py
+++ quest_app/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
-from django.http import HttpResponseRedirect
 from .models import TeachingResource, Class
 from django.http import JsonResponse
 from django.contrib import messages
